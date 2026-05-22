6d228477b4a4c67835998a8ed613c4a3eb6133bd
gerychhh_ <gerkagr29@gmail.com>
2026-05-22 17:39:37 +0300

    add database configuration

--- app/config.py
+++ app/config.py
@@ -7,5 +7,6 @@ load_dotenv()
 class Settings:
     openweathermap_api_key: str | None = os.getenv("OPENWEATHERMAP_API_KEY")
     openweathermap_api_url: str = "https://api.openweathermap.org/data/2.5/weather"
+    database_url: str | None = os.getenv("DATABASE_URL")
 
 settings = Settings()
