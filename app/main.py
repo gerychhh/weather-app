af7c781bb710cf322f5bebd13a81c27891432f2d
gerychhh_ <gerkagr29@gmail.com>
2026-05-22 15:40:46 +0300

    weather search form

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
@@ -8,3 +8,7 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/")
 async def index(request : Request):
     return templates.TemplateResponse(request=request, name="index.html", context={})
+
+@app.post("/weather")
+async def weather(request : Request, city: str = Form(...), unit: str = Form(...)):
+    return templates.TemplateResponse(request=request, name="index.html", context={"city": city, "unit": unit})
