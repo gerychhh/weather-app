767994fa32142df03eb87959205b2786e74c3ad1
gerychhh_ <gerkagr29@gmail.com>
2026-05-22 16:30:30 +0300

    move app settings to config

---
+++ app/config.py
@@ -0,0 +1,11 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    openweathermap_api_key: str | None = os.getenv("OPENWEATHERMAP_API_KEY")
+    openweathermap_api_url: str = "https://api.openweathermap.org/data/2.5/weather"
+
+settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,45 +1,57 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
-from dotenv import load_dotenv
-import os
 import httpx
 from typing import Annotated
-
-load_dotenv()
-
+from .config import settings
 
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
-
 
 @app.get("/")
 async def index(request : Request):
     return templates.TemplateResponse(request=request, name="index.html", context={})
 
 @app.post("/weather")
-async def weather(request : Request, city: str = Form(...), unit: str = Form(...)):
+async def weather(
+    request : Request,
+    city: Annotated[str, Form()],
+    unit: Annotated[str, Form()]
+):
     try:
         weather_data = get_weather_data(city, unit)
         context = {"weather_data": weather_data}
+
     except httpx.HTTPStatusError:
         context = {"error": "City not found. Please try again."}
+
     except httpx.RequestError:
         context = {"error": "Network error. Please try again later."}
+
     return templates.TemplateResponse(request=request, name="index.html", context=context)
 
-def get_weather_data(city: str, unit: str) -> dict:
-    api_key = os.getenv("OPENWEATHERMAP_API_KEY")
-    url = "https://api.openweathermap.org/data/2.5/weather"
+def get_weather_data(
+    city: str,
+    unit: str
+) -> dict[str, str | float]:
+    api_key = settings.openweathermap_api_key
 
-    response = httpx.get(url,
-                          params={"q": city, "units": unit, "appid": api_key}, timeout=10)
+    if not api_key:
+        raise RuntimeError("API key not found. Please set the OPENWEATHERMAP_API_KEY environment variable.")
+    
+    url = settings.openweathermap_api_url
+
+    response = httpx.get(
+        url,
+        params={"q": city, "units": unit, "appid": api_key},
+        timeout=10)
+    
     response.raise_for_status()
     data = response.json()
     
     return {
-        "city" : data["name"],
-        "temperature" : data["main"]["temp"],
-        "description" : data["weather"][0]["description"],
-        "unit" : unit,
+        "city": data["name"],
+        "temperature": data["main"]["temp"],
+        "description": data["weather"][0]["description"],
+        "unit": unit,
     }
